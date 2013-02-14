4497d88850a178f88e97ae785f8e3caf4f7f5c7a
Pau Fracés <pfraces@gmail.com>
2013-02-14 22:57:30 +0100

    initial release

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
 module.exports = actor;
 
-var actor = function (pos) {
-  return new Actor(pos);
+var actor = function (pos, members) {
+  return new Actor(pos, members);
 };
 
-var Actor = function (pos) {
+var Actor = function (pos, members) {
   var self = this;
-  self.pos.x = pos.x;
-  self.pos.y = pos.y;
+  self.x = pos.x;
+  self.y = pos.y;
+  self.last = {
+    x: pos.x,
+    y: pos.y
+  };
+
   self.members = [];
+  members.forEach(function (member) {
+    addMember(self, member);
+  });
+
   self.move = {
     to: function (pos) {
       moveActor(self, pos);
-    }
+    },
+    up: function () {
+      moveActor(self, { self.x, self.y - 1 });
+    },
+    down: function () {
+      moveActor(self, { self.x, self.y + 1 });
+    },
+    left: function () {
+      moveActor(self, { self.x - 1, self.y });
+    },
+    right: function () {
+      moveActor(self, { self.x + 1, self.y });
+    },
   }
 };
 
-Actor.prototype.add = function (el, pos) {
-  this.members.push({
-    el: el,
-    pos: {
-      x: pos.x,
-      y: pos.y
-    }
-  });
+Actor.prototype.add = function (member) {
+  addMember(this, member);
   return this;
 };
 
-moveActor = function (actor, pos) {
-  actor.pos.x = pos.x;
-  actor.pos.y = pos.y;
+var addMember = function (actor, member) {
+  actor.members.push({
+    el: member.el,
+    x: member.x,
+    y: member.y
+  });
+};
+
+var moveActor = function (actor, pos) {
+  actor.last.x = actor.x;
+  actor.last.y = actor.y;
+  actor.x = pos.x;
+  actor.y = pos.y;
 };
