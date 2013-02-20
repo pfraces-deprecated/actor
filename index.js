1ccca2301aa06d84fdfb679471730b5148a43271
Pau Fracés <pfraces@gmail.com>
2013-02-21 00:34:10 +0100

    [refactor] xy

--- index.js
+++ index.js
@@ -1,54 +1,30 @@
-var id = require('uid')(),
+var uid = require('uid'),
     arrayize = require('arrayize'),
-    dictionary = require('dictionary');
+    dictionary = require('dictionary'),
+    xy = require('xy');
+
+var id = uid();
 
 module.exports = function (pos, members) {
   return new Actor(pos, members);
 };
 
 var Actor = function (pos, members) {
-  var self = this;
+  var self = this,
+      pos = xy();
+
   this.id = id();
-  this.actions = dictionary();
+
   this.x = pos.x;
   this.y = pos.y;
-
-  this.last = {
-    x: pos.x,
-    y: pos.y
-  };
+  this.move = pos.move;
   
   this.members = [];
   arrayize(members).forEach(function (member) {
     addMember(self, member);
   });
 
-  this.move = {
-    to: function (pos) {
-      self.x = pos.x;
-      self.y = pos.y;
-    },
-    back: function () {
-      self.x = self.last.x; 
-      self.y = self.last.y;
-    },
-    up: function () {
-      self.x = self.x;
-      self.y = self.y - 1;
-    },
-    down: function () {
-      self.x = self.x;
-      self.y = self.y + 1;
-    },
-    left: function () {
-      self.x = self.x - 1; 
-      self.y = self.y;
-    },
-    right: function () {
-      self.x = self.x + 1;
-      self.y = self.y;
-    }
-  }
+  this.actions = dictionary();
 };
 
 Actor.prototype.add = function (member) {
